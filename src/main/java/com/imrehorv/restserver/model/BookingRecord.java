e715c80556c789a33767e2015b4bb60611ce46cd
Imre Horvath <imrehorv@gmail.com>
2019-09-16 20:02:14 +0200

    serialize localdatetime

--- src/main/java/com/imrehorv/restserver/model/BookingRecord.java
+++ src/main/java/com/imrehorv/restserver/model/BookingRecord.java
@@ -2,9 +2,23 @@ package com.imrehorv.restserver.model;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 public class BookingRecord {
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
 	private LocalDateTime startDate;
+	
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
 	private LocalDateTime endDate;
+	
     private String userid;
     private String username;
     private String roomid;
