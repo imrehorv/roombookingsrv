42272c771aeae214a83004240be72ad9bf577c52
Imre Horvath <imrehorv@gmail.com>
2019-09-18 19:40:10 +0200

    implemented store und load

--- src/main/java/com/imrehorv/restserver/BookingResource.java
+++ src/main/java/com/imrehorv/restserver/BookingResource.java
@@ -1,11 +1,16 @@
 package com.imrehorv.restserver;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -26,6 +31,18 @@ public class BookingResource {
 		logger.info("store called bookingRecord:"+bookingRecord);
 		bookingRepo.store(bookingRecord);
 		return Response.accepted().entity(bookingRecord).build();
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("{date}")
+	public Response load(@PathParam("date") String datestr)
+	{
+		logger.info("load called date:"+datestr);
+		LocalDate date=LocalDate.parse(datestr);
+		logger.info("parsed date:"+date);
+		List<BookingRecord> result=bookingRepo.load(date);
+		return Response.accepted().entity(result).build();
 	}
 
 }
--- src/main/java/com/imrehorv/restserver/persistence/BookingRepo.java
+++ src/main/java/com/imrehorv/restserver/persistence/BookingRepo.java
@@ -1,5 +1,11 @@
 package com.imrehorv.restserver.persistence;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
@@ -11,8 +17,58 @@ public class BookingRepo {
 	
 	Logger logger=Logger.getLogger(BookingRepo.class.getName());
 	
+	private Map<LocalDate,Map<LocalDateTime,Map<String,BookingRecord>>> map=new HashMap<>();
+	
 	public void store(BookingRecord record) {
 		logger.info("store called record:"+record);
-		
+		LocalDate key=record.getStartDate().toLocalDate();
+		Map<LocalDateTime,Map<String,BookingRecord>> innermap=map.get(key);
+		if (innermap==null)
+		{
+			innermap=new HashMap<LocalDateTime,Map<String,BookingRecord>>();
+			map.put(key, innermap);
+		}
+		Map<String,BookingRecord> roommap=innermap.get(record.getStartDate());
+		if (roommap==null)
+		{
+			roommap=new HashMap<>();
+			innermap.put(record.getStartDate(), roommap);
+		}
+		BookingRecord storedrecord=roommap.get(record.getRoomid());
+		if (record.getUserid()==null)
+		{
+			roommap.put(record.getRoomid(),null);
+		}
+		else if (storedrecord==null)
+		{
+			roommap.put(record.getRoomid(), record);
+		}
+		debug();
+	}
+	
+	public List<BookingRecord> load(LocalDate date) {
+		Map<LocalDateTime,Map<String,BookingRecord>> innermap=map.get(date);
+		List<BookingRecord> result=new ArrayList<>();
+		if (innermap!=null)
+		{
+			for (Map<String,BookingRecord> recmap:innermap.values())
+			{
+				if (recmap!=null)
+				{
+					for (BookingRecord record:recmap.values())
+					{
+						if (record!=null)
+						{
+							result.add(record);
+						}
+					}
+				}
+			}
+		}
+		return result;
+	}
+	
+	private void debug() {
+		logger.info("map after save:"+map);
 	}
 }
